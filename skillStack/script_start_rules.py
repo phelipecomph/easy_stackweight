35886ab3bcb8e4837593939440c9b6095e8ba9fe
Phelipe Müller <65641281+phelipecomph@users.noreply.github.com>
2024-11-28 16:11:02 -0300

    Revert "new rule and edit rules pages"

--- skillStack/script_start_rules.py
+++
@@ -1,9 +0,0 @@
-import json
-
-RULES = [
-    {"habilidade": "habilidade_teste", "regra": "lambda vars: (vars.get('trecho_outro_genero_9') == 0)", "peso": 10},
-    {"habilidade": "outra_habilidade_teste", "regra": "lambda vars: (vars.get('trecho_outro_genero_9') == 0) & (vars.get('num_pontuacao_eixo_2')>=120)", "peso": 5},
-]
-
-with open("rules.json", "w") as f:
-    json.dump(RULES, f)
